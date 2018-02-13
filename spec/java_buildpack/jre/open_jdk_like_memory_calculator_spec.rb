21ecb3c762b90bddcb0038f71040d5e2b4f5f90e
Josep Cano Puig <jcanopui@everis.com>
2018-02-13 11:04:31 +0100

    disable memory calculator

--- spec/java_buildpack/jre/open_jdk_like_memory_calculator_spec.rb
+++ spec/java_buildpack/jre/open_jdk_like_memory_calculator_spec.rb
@@ -84,7 +84,7 @@ describe JavaBuildpack::Jre::OpenJDKLikeMemoryCalculator do
 
     command = component.memory_calculation_command
 
-    expect(command).to eq('')
+    expect(command).to eq('JAVA_OPTS="$JAVA_OPTS -Xms128M -Xmx256M"')
   end
 
   it 'does not throw an error when a directory ends in .jar',
